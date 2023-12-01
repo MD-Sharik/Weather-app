9cc5e47a7f7b2c5e0fcf0ca84134f29a9008dc73
MD Sharik <md.sharik5012@gmail.com>
2023-12-02 02:42:48 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const temperatureIcon = document.getElementById("temperatureicon")
+const humid = document.getElementById("humid");
+const inputsearch = document.getElementById("search");
+const searchbtn = document.getElementById("smbtn");
+const temprature = document.getElementById("temp");
+const Wind = document.getElementById("wind");
+const animation = document.getElementById("offanim");
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '19acb71371mshab2a6900569a4ccp1c83d7jsn8250a7ed6f5a',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const postData = (city)=>{
+
+fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options).then(response=> response.json()).then(response=>{
+	console.log(response);
+	humid.innerText = response.humidity || "💀"
+	temprature.innerText = response.temp || "💀"
+	Wind.innerText = response.wind_speed || "💀"
+
+	if(response.temp > 35){
+		temperatureIcon.src = "https://img.icons8.com/emoji/96/hot-face.png"
+	}
+	else if(response.temp >  24){
+		temperatureIcon.src = "https://img.icons8.com/emoji/96/melting-face-emoji.png"
+	}
+	else if(response.temp >  14){
+		temperatureIcon.src = "https://img.icons8.com/emoji/96/sneezing-face.png"
+	}else if (response.temp != undefined){
+		temperatureIcon.src = "https://img.icons8.com/emoji/96/cold-face.png"
+	}
+	else temperatureIcon.src = "https://img.icons8.com/emoji/96/question-mark-emoji.png"
+	
+}).catch(err=>console.error(err))
+}
+
+animation.addEventListener("click",()=>{
+	document.querySelectorAll(".animate-bounce").forEach((item)=>{
+		if(item.style.animationPlayState == "paused"){
+			animation.innerText = "Play" 
+			item.style.animationPlayState = "running"
+		}else{
+			animation.innerText = "Pause"
+			item.style.animationPlayState =	 "paused"
+		}
+	})
+})
+
+searchbtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	postData(inputsearch.value);
+
+})
+
+
+
